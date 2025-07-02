9557e1c4ba2b6601d5ad1dc7ab7cdbdb311d3f9a
Grswld <tanner.k.alvarez@protonmail.com>
2025-07-02 15:44:02 -0600

    ...

--- 001 - Automatic Budget/main.tf
+++ 001 - Automatic Budget/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_budgets_budget" "terraform-griswold" {
   name              = "terraform budget"
   budget_type       = "COST"
